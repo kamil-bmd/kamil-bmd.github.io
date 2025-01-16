c930529abee0dca0c93e08a063865b65576b58b8
kamil⭐🌀 <kamil.bmddne@gmail.com>
2025-01-16 16:34:58 +0100

    correction

--- Jour4/FlexBox-Transition/script.js
+++ Jour4/FlexBox-Transition/script.js
@@ -2,12 +2,10 @@ const menuToggle = document.getElementById('menu-toggle');
 const mobileMenu = document.getElementById('mobile-menu');
 const closeMenu = document.getElementById('close-menu');
 
-// Ouvrir le menu mobile
 menuToggle.addEventListener('click', () => {
     mobileMenu.style.display = 'flex';
 });
 
-// Fermer le menu mobile
 closeMenu.addEventListener('click', () => {
     mobileMenu.style.display = 'none';
 });
